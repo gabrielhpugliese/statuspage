dccf1328dfed1f9e930601abecff189c67f08a16
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2015-05-23 17:10:30 -0300

    Add incidents component and refactor system-list to systems

---
+++ client/components/incidents/incidents.jsx
@@ -0,0 +1,44 @@
+/* @jsx React.DOM */
+'use strict';
+
+var IncidentAction = React.createClass({
+  render: function () {
+    return (
+      <div className="incident-action">
+        <div className="incident-action__name">
+          Completed -
+        </div>
+        <div className="incident-action__description">
+          We are going to perform scheduled database maintenance on travis-ci.org. This is expected to take less than an hour. The site and API will be unavailable during this time. Incoming build requests will still be picked up, and processed later.
+        </div>
+        <div className="incident-action__date">
+          May 23, 09:36 UTC
+        </div>
+      </div>
+    );
+  }
+});
+
+var Incidents = ReactMeteor.createClass({
+  templateName: 'Incidents',
+
+  render: function () {
+    return (
+      <div className="incidents">
+        <h4>Past Incidents</h4>
+        <div className="incidents__item">
+          <div className="incidents__date">
+            May 23, 2015
+          </div>
+          <hr/>
+
+          <div className="incidents__title">
+            Database maintenance
+          </div>
+
+          <IncidentAction />
+        </div>
+      </div>
+    );
+  }
+});
---
+++ client/components/systems/systems.jsx
@@ -0,0 +1,45 @@
+/* @jsx React.DOM */
+'use strict';
+
+var Systems = ReactMeteor.createClass({
+  templateName: 'Systems',
+
+  render: function () {
+    return (
+      <div className="systems">
+        <div className="systems__item">
+          <div className="systems__system-name">
+          API
+          </div>
+          <div className="systems__system-status">
+            Operational
+          </div>
+        </div>
+        <div className="systems__item">
+          <div className="systems__system-name">
+          API
+          </div>
+          <div className="systems__system-status">
+            Operational
+          </div>
+        </div>
+        <div className="systems__item">
+          <div className="systems__system-name">
+          API
+          </div>
+          <div className="systems__system-status">
+            Operational
+          </div>
+        </div>
+        <div className="systems__item">
+          <div className="systems__system-name">
+          API
+          </div>
+          <div className="systems__system-status">
+            Operational
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
