f8147613c027eb3bf7d3ef0fb84235f8d7e3dc69
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2015-05-24 19:24:55 -0300

    Fix the ping loop

--- packages/system-pinger/main.js
+++ packages/system-pinger/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-Meteor.setTimeout(function () {
-  Systems.find().forEach(function (doc) {
+Meteor.setInterval(function () {
+  SystemsColl.find().forEach(function (doc) {
     console.log('Pinging', doc.name);
     SystemPinger.pingAndSave(doc.name);
   });
