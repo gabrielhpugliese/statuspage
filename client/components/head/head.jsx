00c5d3b08494004edb7d49d28d065be0d6ff4ec1
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2015-05-27 17:16:42 -0300

    Add subscribe button functionality - needs to implement sending on server

--- client/components/head/head.jsx
+++ client/components/head/head.jsx
@@ -1,14 +1,44 @@
 /* @jsx React.DOM */
 'use strict';
 
-var SubscribeStrip = React.createClass({
+var SubscribeBox = React.createClass({
   propTypes: {
-    title: React.PropTypes.string.isRequired
+    visible: React.PropTypes.bool.isRequired,
+    onSubmit: React.PropTypes.func.isRequired
   },
 
   getDefaultProps: function () {
     return {
-      title: ''
+      visible: false,
+      onSubmit: null
+    };
+  },
+
+  render: function () {
+    var style = {
+      display: this.props.visible ? 'block' : 'none'
+    };
+
+    return (
+      <form className="head__subscribe-box" style={style} onSubmit={this.props.onSubmit}>
+        Get notifications when there is an update or incident.
+        <input className="head__subscribe-input" placeholder="email"></input>
+        <button className="head__subscribe-button--full">Subscribe via email</button>
+      </form>
+    );
+  }
+});
+
+var SubscribeStrip = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string.isRequired,
+    onSubscribeClick: React.PropTypes.func.isRequired
+  },
+
+  getDefaultProps: function () {
+    return {
+      title: '',
+      onSubscribeClick: null
     };
   },
 
@@ -16,7 +46,7 @@ var SubscribeStrip = React.createClass({
     return (
       <div className="head__subscribe-strip">
         <div className="head__title">{this.props.title}</div>
-        <button className="head__subscribe-button">Subscribe to updates</button>
+        <button onClick={this.props.onSubscribeClick} className="head__subscribe-button">Subscribe to updates</button>
       </div>
     );
   }
@@ -46,6 +76,23 @@ var Head = ReactMeteor.createClass({
     };
   },
 
+  getInitialState: function () {
+    return {
+      subscribeBoxVisible: false
+    };
+  },
+
+  onSubscribeClick: function () {
+    this.setState({
+      subscribeBoxVisible: ! this.state.subscribeBoxVisible
+    });
+  },
+
+  onSubscribeSubmit: function (event) {
+    event.preventDefault();
+    debugger;
+  },
+
   render: function () {
     var style = {
       backgroundImage: 'url(' + this.props.imageUrl + ')'
@@ -53,7 +100,8 @@ var Head = ReactMeteor.createClass({
 
     return (
       <div className="head" style={style}>
-        <SubscribeStrip title={this.props.title} />
+        <SubscribeStrip title={this.props.title} onSubscribeClick={this.onSubscribeClick}/>
+        <SubscribeBox visible={this.state.subscribeBoxVisible} onSubmit={this.onSubscribeSubmit} />
       </div>
     );
   }
